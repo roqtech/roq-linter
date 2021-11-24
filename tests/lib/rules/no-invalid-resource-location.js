b78e43eb2fcf65b7c810f554474bf749c744c7f6
udyansharma-01 <82495949+udyansharma-01@users.noreply.github.com>
2021-11-24 12:34:19 +0530

    feat: convention to disallow components at invalid locations (#8)
    * feat: convention to disallow components at invalid locations

--- tests/lib/rules/no-invalid-resource-location.js
+++ tests/lib/rules/no-invalid-resource-location.js
@@ -29,26 +29,39 @@ ruleTesterInstance.run('no-invalid-resource-location', ruleUnderTest, {
       filename: 'frontend/src/modules/common/components/notification/notification.styles.ts',
     },
     {
-      code: `export interface NotificationClasses {}
-      export const useNotificationStyles = ()=>{/* definition */}`,
+      code: 'export const DropPartial = ()=>{/* definition */} // File Path : frontend/src/views/files/partials/drop/drop.partial.ts',
       filename: 'frontend/src/views/files/partials/drop/drop.partial.ts',
+    },
+    {
+      code: 'export const DropPartial = ()=>{/* definition */} // File Path : frontend/src/layouts/layA/partials/drop/drop.partial.ts',
+      filename: 'frontend/src/layouts/layA/partials/drop/drop.partial.ts',
+    },
+    {
+      code: 'export const DropPartial = ()=>{/* definition */} // File Path : frontend/src/modules/modA/components/comp1/partials/drop/drop.partial.ts',
+      filename: 'frontend/src/modules/modA/components/comp1/partials/drop/drop.partial.ts',
     },
   ],
   invalid: [
     {
-      code: 'export const useAuth = ()=>{/* definition */}',
+      code: 'export const dialog = ()=>{/* definition */}',
       errors: [
         {
           message: 'All components and their related styles should be placed in their own directory (.../components/{component-name}/{component-name}.component.tsx)',
+        },
+        {
+          message: 'The file-name and the parent directory name should match',
         },
       ],
       filename: 'frontend/src/modules/common/components/dialog.component.tsx',
     },
     {
-      code: 'export const useAuth = ()=>{/* definition */}',
+      code: 'export const dialogPartial = ()=>{/* definition */}',
       errors: [
         {
-          message: 'All partials and their related styles should be placed either in their own directory (.../partials/{partial-name}/{partial-name}.partial.tsx) or alongwith a related component in their directory',
+          message: 'All partials and their related styles should be placed either in their own directory (.../partials/{partial-name}/{partial-name}.partial.tsx)',
+        },
+        {
+          message: 'The file-name and the parent directory name should match',
         },
       ],
       filename: 'frontend/src/modules/common/components/dialog.partial.tsx',
@@ -63,13 +76,40 @@ ruleTesterInstance.run('no-invalid-resource-location', ruleUnderTest, {
       filename: 'frontend/src/modules/common/components/notification/notifications.component.tsx',
     },
     {
-      code: '// File Path : frontend/src/modules/common/components/notification/notifications.component.tsx',
+      code: '// File Path : frontend/src/modules/components/notifications/notifications.styles.tsx',
+      errors: [
+        {
+          message: 'Components directory can only exist at the following path pattern modules/*/components',
+        },
+      ],
+      filename: 'frontend/src/modules/components/notifications/notifications.styles.tsx',
+    },
+    {
+      code: '// File Path : frontend/src/layouts/main/components/notification/notifications.component.tsx',
       errors: [
         {
           message: 'Layouts should have partials instead of components. A good start to fix this could be to rename super parent "components" dir to "partials"',
         },
+        {
+          message: 'All components and their related styles should be placed in their own directory (.../components/{component-name}/{component-name}.component.tsx)',
+        },
+        {
+          message: 'The file-name and the parent directory name should match',
+        },
+        {
+          message: 'Components directory can only exist at the following path pattern modules/*/components',
+        },
       ],
       filename: 'frontend/src/layouts/main/components/notification/notifications.component.tsx',
+    },
+    {
+      code: '// File Path : frontend/src/layouts/main/partials/notification/notification.tsx',
+      errors: [
+        {
+          message: 'The partials directory can only exist at the path pattern [views, layouts, components]/*/partials',
+        },
+      ],
+      filename: 'frontend/src/modules/partials/notification/notification.tsx',
     },
   ],
 });
